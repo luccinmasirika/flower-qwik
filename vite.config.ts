efb6fce8e900d4e7fed03267fa969c3432c90ae0
Luccin Masirika <53916378+luccin243@users.noreply.github.com>
2022-08-28 02:50:36 +0100

    host

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 
-// @ts-ignore
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [qwikVite()],
     server: {
-      port: env.PORT || 3000,
+      port: +env.PORT || 3000,
     },
   };
 });
